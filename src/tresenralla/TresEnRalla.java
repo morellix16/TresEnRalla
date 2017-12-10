e45bec04dd2d8951a303b48c445e27d4f2868de8
morellix16 <mark.morelli16@gmail.com>
2017-12-10 17:00:39 +0100

    Creacion de metodos inicializar e imprimir tablero

--- src/tresenralla/TresEnRalla.java
+++ src/tresenralla/TresEnRalla.java
@@ -14,14 +14,29 @@ public class TresEnRalla {
     /**
      * @param args the command line arguments
      */
+    static Tablero tablero = new Tablero(new String[3][3]);
+    static Player player1 = new Player("player 1");
+    static Player player2 = new Player("player 2");
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        Player player1 = new Player("player 1");
-        Player player2 = new Player("player 2");
-        
-        Tablero tablero = new Tablero(new String [3][3]);
-        
+       inicializarTablero();
+       imprimirTablero();
+    }
+
+    public static void inicializarTablero() {
+        for (int i = 0; i < tablero.getTablero().length; i++) {
+            for (int j = 0; j < tablero.getTablero()[i].length; j++) {
+                tablero.getTablero()[i][j] = "A";
+            }
+        }
     }
     
+    public static void imprimirTablero() {
+        for (int i = 0; i < tablero.getTablero().length; i++) {
+            System.out.println("");
+            for (int j = 0; j < tablero.getTablero()[i].length; j++) {
+                System.out.print(tablero.getTablero()[i][j] + " ");
+            }
+        }
+    }
 }
